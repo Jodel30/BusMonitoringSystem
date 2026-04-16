56bc171fd7a9b77a97d230b81580f178dbf24a62
Jodel B. Marande <jodmarande@gmail.com>
2026-04-16 17:02:45 +0800

    yoww

--- MonitoringSystem/Controllers/AccountController.cs
+++ MonitoringSystem/Controllers/AccountController.cs
@@ -4,8 +4,20 @@ namespace MonitoringSystem.Controllers
 {
     public class AccountController : Controller
     {
-        public IActionResult Index()
+        public IActionResult Login()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(string username, string password)
+        {
+            if (username == "admin" && password == "1234")
+            {
+                return RedirectToAction("Index", "LguDashboard");
+            }
+
+            ViewBag.Error = "Invalid login";
             return View();
         }
     }
