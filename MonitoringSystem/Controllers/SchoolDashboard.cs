3cbfb7af448e89796d1cf03374810c0f23df1969
Jodel B. Marande <jodmarande@gmail.com>
2026-04-19 20:00:53 +0800

    yowww

---
+++ MonitoringSystem/Controllers/SchoolDashboard.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MonitoringSystem.Controllers
+{
+    public class SchoolDashboard : Controller
+    {
+        public IActionResult SchoolAdmin()
+        {
+            return View();
+        }
+    }
+}
---
+++ MonitoringSystem/wwwroot/js/School.js
@@ -0,0 +1,20 @@
+﻿function previewFile() {
+    const preview = document.getElementById('previewImg');
+    const icon = document.getElementById('placeholderIcon');
+    const file = document.getElementById('studentPhoto').files[0];
+    const reader = new FileReader();
+
+    reader.onloadend = function () {
+        preview.src = reader.result;
+        preview.style.display = "block"; // Show image
+        icon.style.display = "none";     // Hide icon
+    }
+
+    if (file) {
+        reader.readAsDataURL(file);
+    } else {
+        preview.src = "";
+        preview.style.display = "none";
+        icon.style.display = "block";
+    }
+}
